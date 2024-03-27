dbe101a98fc34504aadca557693d32c9591dd1d3
Ngan ne <watanabilinlin@gmail.com>
2024-03-27 18:08:40 +0700

    Update: update ViewModel for add, delete, update, search event

---
+++ Commands/RelayCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input; //import the ICommand interface from the System.Windows.Input namespace
+
+namespace test2.Commands
+{
+    public class RelayCommand : ICommand //implement the ICommand interface
+    {
+        public event EventHandler? CanExecuteChanged;
+        private Action DoWork; //create a private field of type Action
+        public RelayCommand(Action work) //create a constructor that takes an Action parameter
+        {
+            DoWork = work; //initialize the DoWork field
+        }
+       
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            DoWork();
+        }
+    }
+}
--- ViewModels/EmployessViewModel.cs
+++ ViewModels/EmployessViewModel.cs
@@ -5,11 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
 using test2.Models; //import the Employee class from the Models namespace
+using test2.Commands; //import the RelayCommand class from the Commands namespace
+using System.Collections.ObjectModel;
 
 namespace test2.ViewModels
 {
     public class EmployessViewModel : INotifyPropertyChanged
     {
+        #region INotifyPropertyChanged_Implementation
         public event PropertyChangedEventHandler PropertyChanged; //implement the PropertyChangedEventHandler event
         private void OnPropertyChanged(string propertyName) //implement the OnPropertyChanged method
         {
@@ -18,16 +21,23 @@ namespace test2.ViewModels
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
             }
         }
-        EmployeeService employeeService; //create an instance of the EmployeeService class
+        #endregion
+
+        EmployeeService ObjEmployeeService; //create an instance of the EmployeeService class
         public EmployessViewModel()
         {
-            employeeService = new EmployeeService(); //initialize the employeeService object
+            ObjEmployeeService = new EmployeeService(); //initialize the employeeService object
             LoadData(); //call the LoadData method
+            CurrentEmployee = new Employee(); 
+            addCommand = new RelayCommand(Add); //initialize the addCommand object
+            searchCommand = new RelayCommand(Search);
+            deleteCommand = new RelayCommand(Delete);
+            updateCommand = new RelayCommand(Update);
         }
-        
 
-        private List<Employee> employeesList;
-        public List<Employee> EmployeesList //implement the EmployeesList property
+        #region DisplayOperation
+        private ObservableCollection<Employee> employeesList;
+        public ObservableCollection<Employee> EmployeesList //implement the EmployeesList property
         {
             get { return employeesList; }
             set
@@ -38,8 +48,147 @@ namespace test2.ViewModels
         }
         private void LoadData()
         {
-            EmployeesList = employeeService.GetAll(); //call the GetAll method of the employeeService object
+            //ObservableCollection list is used to update the UI automatically
+            EmployeesList = new ObservableCollection<Employee>(ObjEmployeeService.GetAll()); //call the GetAll method of the employeeService object
         }
+        #endregion
+
+        private Employee currentEmployee;
+        public Employee CurrentEmployee //implement the CurrentEmployee property
+        {
+            get { return currentEmployee; }
+            set
+            {
+                currentEmployee = value;
+                OnPropertyChanged("CurrentEmployee");
+            }
+        }
+        private string message;
+        public string Message
+        {
+            get { return message; }
+            set { message = value; OnPropertyChanged("Message"); }
+        }
+
+        #region AddOperation
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get { return addCommand; }
+        }
+
+        public void Add()
+        {
+            try
+            {
+                var IsAdded = ObjEmployeeService.Add(currentEmployee);
+                LoadData();
+                if (IsAdded)
+                {
+                    Message = "Employee added successfully";
+                }
+                else
+                {
+                    Message = "Add operation failed";
+                }
+            }
+            catch (Exception ex)
+            {
+               Message = ex.Message;
+            }
+        }
+        #endregion
+
+        #region SearchOperation
+
+        RelayCommand searchCommand;
+        public RelayCommand SearchCommand
+        {
+            get { return searchCommand; }
+        }
+        public void Search()
+        {
+            try
+            {
+                var ObjEmployee = ObjEmployeeService.Search(currentEmployee.Id);
+                if (ObjEmployee != null)
+                {
+                    CurrentEmployee.Name = ObjEmployee.Name;
+                    CurrentEmployee.Age = ObjEmployee.Age;
+                    Message = "Employee found";
+                }
+                else
+                {
+                    Message = "Employee not found";
+                }
+            }
+            catch (Exception ex)
+            {
+                Message = ex.Message;
+            }
+        }
+
+        #endregion
+
+        #region DeleteOperation
+
+        private RelayCommand deleteCommand;
+        public RelayCommand DeleteCommand
+        {
+            get { return deleteCommand; }
+        }
+        public void Delete()
+        {
+            try
+            {
+                var IsDeleted = ObjEmployeeService.Delete(currentEmployee.Id);
+                
+                if (IsDeleted)
+                {
+                    Message = "Employee deleted successfully";
+                    LoadData();
+                }
+                else
+                {
+                    Message = "Delete operation failed";
+                }
+            }
+            catch (Exception ex)
+            {
+                Message = ex.Message;
+            }
+        }
+        #endregion
+
+        #region UpdateOperation
+        RelayCommand updateCommand;
+        public RelayCommand UpdateCommand
+        {
+            get { return updateCommand; }
+        }
+
+        public void Update()
+        {
+            try
+            {
+                var IsUpdated = ObjEmployeeService.Update(currentEmployee);
+                if (IsUpdated)
+                {
+                    Message = "Employee updated successfully";
+                    LoadData();
+                }
+                else
+                {
+                    Message = "Update operation failed";
+                }
+            }
+            catch (Exception ex)
+            {
+                Message = ex.Message;
+            }
+        }
+        #endregion
+
 
     }
 }
